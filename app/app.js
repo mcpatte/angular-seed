ced8c6450a0d53744be931fa07d0067905b21198
Chase Patterson <mcpatte@gmail.com>
2017-02-06 16:15:32 -0800

    add assets and set display sprites using easleJS

--- app/app.js
+++ app/app.js
@@ -4,10 +4,9 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  'myApp.version',
+  'myApp.directives'])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
